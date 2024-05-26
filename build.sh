7cdec9e946e1fe5afc903cd08041829a335fa272
Errick Miron <highsman@lilaverse.app>
2024-05-26 13:37:46 -0700

    added procfile

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ if ! command -v swift &> /dev/null; then
     echo "Swift could not be found, installing..."
     SWIFT_VERSION=5.4.2-RELEASE
     SWIFT_PLATFORM=ubuntu20.04
-    SWIFT_URL=https://swift.org/builds/swift-$SWIFT_VERSION/$SWIFT_PLATFORM/swift-$SWIFT_VERSION/swift-$SWIFT_VERSION-$SWIFT_PLATFORM.tar.gz
+    SWIFT_URL=https://swift.org/builds/swift-5.4.2-release/ubuntu2004/swift-5.4.2-RELEASE/swift-5.4.2-RELEASE-ubuntu20.04.tar.gz
 
     curl -fSL $SWIFT_URL -o swift.tar.gz
     if [ $? -ne 0 ]; then
@@ -13,13 +13,14 @@ if ! command -v swift &> /dev/null; then
         exit 1
     fi
 
-    tar -xzf swift.tar.gz --strip-components=1
+    mkdir swift
+    tar -xzf swift.tar.gz -C swift --strip-components=1
     if [ $? -ne 0 ]; then
         echo "Failed to extract Swift tarball."
         exit 1
     fi
 
-    export PATH=$(pwd)/usr/bin:"${PATH}"
+    export PATH=$(pwd)/swift/usr/bin:"${PATH}"
     if ! command -v swift &> /dev/null; then
         echo "Swift installation failed."
         exit 1
